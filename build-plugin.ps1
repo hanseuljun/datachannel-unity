d64073d38762cab987088d471d7733025ad90847
Hanseul Jun <hanseuljun@gmail.com>
2020-10-04 17:45:09 -0700

    Add msvcp and vcruntime dlls.

--- build-plugin.ps1
+++ build-plugin.ps1
@@ -24,18 +24,33 @@ $x86Path = "$buildPath\x86\src\$configuration"
 $x64Path = "$buildPath\x64\src\$configuration"
 $uwpPluginPath = "$packagePath\Runtime\Plugins\UWP\x86"
 $editorPath = "$packagePath\Editor\Plugins"
+$binPath = (Get-Location).path + "\unity\bin"
 
 Copy-Item "$x86Path\DataChannelUnity.dll" -Destination $uwpPluginPath
-Copy-Item "$x64Path\DataChannelUnity.dll" -Destination $editorPath
-
-$binPath = (Get-Location).path + "\unity\bin"
 Copy-Item "$binPath\DataChannelUnity.dll.meta" -Destination $uwpPluginPath
-Copy-Item "$binPath\libcrypto-1_1.dll.meta" -Destination $uwpPluginPath
-Copy-Item "$binPath\libssl-1_1.dll.meta" -Destination $uwpPluginPath
+
+if ($debug -eq $false) {
+	Copy-Item "$binPath\msvcp140.dll" -Destination $uwpPluginPath
+	Copy-Item "$binPath\msvcp140.dll.meta" -Destination $uwpPluginPath
+
+	Copy-Item "$binPath\vcruntime140.dll" -Destination $uwpPluginPath
+	Copy-Item "$binPath\vcruntime140.dll.meta" -Destination $uwpPluginPath
+} else {
+	Copy-Item "$binPath\msvcp140d.dll" -Destination $uwpPluginPath
+	Copy-Item "$binPath\msvcp140d.dll.meta" -Destination $uwpPluginPath
+
+	Copy-Item "$binPath\vcruntime140d.dll" -Destination $uwpPluginPath
+	Copy-Item "$binPath\vcruntime140d.dll.meta" -Destination $uwpPluginPath
+}
 
 Copy-Item "$x86Path\libcrypto-1_1.dll" -Destination $uwpPluginPath
+Copy-Item "$binPath\libcrypto-1_1.dll.meta" -Destination $uwpPluginPath
+
+Copy-Item "$binPath\libssl-1_1.dll.meta" -Destination $uwpPluginPath
 Copy-Item "$x86Path\libssl-1_1.dll" -Destination $uwpPluginPath
 
+
+Copy-Item "$x64Path\DataChannelUnity.dll" -Destination $editorPath
 Copy-Item "$x64Path\libcrypto-1_1-x64.dll" -Destination $editorPath
 Copy-Item "$x64Path\libssl-1_1-x64.dll" -Destination $editorPath
 
