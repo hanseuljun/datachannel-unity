00ede1abd655f7d1a26748845d850b81d87afaf7
Hanseul Jun <hanseuljun@gmail.com>
2020-10-28 23:48:22 -0700

    Add RSA_KEY_BITS_2048 with an updated libdatachannel.

--- cmake-vs.ps1
+++ cmake-vs.ps1
@@ -3,7 +3,7 @@ New-Item .\build\x86-uwp -ItemType Directory
 New-Item .\build\x64 -ItemType Directory
 
 $CMakeToolchainFile = (Get-Location).path + "\vcpkg\scripts\buildsystems\vcpkg.cmake"
-cmake -B .\build\x86-uwp -G "Visual Studio 16 2019" -A Win32 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0" -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DNO_EXAMPLES=ON -DNO_TESTS=ON -DCAPI_STDCALL=ON
+cmake -B .\build\x86-uwp -G "Visual Studio 16 2019" -A Win32 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0" -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DNO_EXAMPLES=ON -DNO_TESTS=ON -DRSA_KEY_BITS_2048=ON -DCAPI_STDCALL=ON
 cmake -B .\build\x64 -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DCAPI_STDCALL=ON
 
 Pause
