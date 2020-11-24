a5beaaa7a5149f8cac1e5c52d33cecf79d56290c
Hanseul Jun <hanseuljun@gmail.com>
2020-11-23 20:42:01 -0800

    update init-vcpkg

--- init-vcpkg.ps1
+++ init-vcpkg.ps1
@@ -1,5 +1,6 @@
-Start-Process -NoNewWindow -Wait -FilePath .\vcpkg\bootstrap-vcpkg.bat
+# Start-Process -Wait gets stuck sometimes. Using -Passthru with "| Wait-Process" instead.
+Start-Process -Passthru -NoNewWindow -FilePath .\vcpkg\bootstrap-vcpkg.bat | Wait-Process
 
 $vcpkgs = Get-Content -Path .\vcpkgs.txt
 $args = ,"install" + $vcpkgs
-Start-Process -NoNewWindow -Wait -FilePath .\vcpkg\vcpkg.exe -ArgumentList $args
+Start-Process -Passthru -NoNewWindow -FilePath .\vcpkg\vcpkg.exe -ArgumentList $args | Wait-Process
