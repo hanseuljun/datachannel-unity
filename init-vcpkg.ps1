b5b69eb16b5f5dc8d4b3c33cd1ba0bc42fc5c1b4
Hanseul Jun <hanseuljun@gmail.com>
2020-11-23 18:04:42 -0800

    Fix init-vcpkg.ps1.

--- init-vcpkg.ps1
+++ init-vcpkg.ps1
@@ -1,4 +1,5 @@
-#Start-Process -NoNewWindow -FilePath .\vcpkg\bootstrap-vcpkg.bat
+Start-Process -NoNewWindow -Wait -FilePath .\vcpkg\bootstrap-vcpkg.bat
+
 $vcpkgs = Get-Content -Path .\vcpkgs.txt
 $args = ,"install" + $vcpkgs
-Start-Process -NoNewWindow -FilePath .\vcpkg\vcpkg.exe -ArgumentList $args
+Start-Process -NoNewWindow -Wait -FilePath .\vcpkg\vcpkg.exe -ArgumentList $args
