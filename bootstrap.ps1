10e0f0673dac94d890be8e59819a5f298c4e03bd
Hanseul Jun <hanseuljun@gmail.com>
2021-02-09 10:09:01 -0800

    Fix typo.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,3 @@
-Invoke-Expression -Command $PSScriptRoot\cripts\install-vcpkgs.ps1
+Invoke-Expression -Command $PSScriptRoot\scripts\install-vcpkgs.ps1
 Invoke-Expression -Command $PSScriptRoot\scripts\cmake-vs.ps1
 Invoke-Expression -Command $PSScriptRoot\scripts\build-plugin.ps1
