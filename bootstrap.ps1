6760e94d53718f5792aae80576e6c199149bd6a2
Hanseul Jun <hanseuljun@gmail.com>
2021-02-09 10:31:38 -0800

    Add Write-Outputs to bootstrap.ps1.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,8 @@
+Write-Output "Running datachannel-unity\scripts\install-vcpkgs.ps1"
 Invoke-Expression -Command $PSScriptRoot\scripts\install-vcpkgs.ps1
+
+Write-Output "Running datachannel-unity\scripts\cmake-vs.ps1"
 Invoke-Expression -Command $PSScriptRoot\scripts\cmake-vs.ps1
+
+Write-Output "Running datachannel-unity\scripts\build-plugin.ps1"
 Invoke-Expression -Command $PSScriptRoot\scripts\build-plugin.ps1
