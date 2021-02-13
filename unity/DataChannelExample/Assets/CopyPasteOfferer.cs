9044951f5f5545f4c37106b9a466d2c06754f2fa
Hanseul Jun <hanseuljun@gmail.com>
2021-02-12 16:57:23 -0800

    Some more work on the example.

--- unity/DataChannelExample/Assets/CopyPasteOfferer.cs
+++ unity/DataChannelExample/Assets/CopyPasteOfferer.cs
@@ -5,9 +5,18 @@ public class CopyPasteOfferer : MonoBehaviour
 {
     private PeerConnection peerConnection;
     private DataChannel dataChannel;
+    private string localDescriptionStr;
+    private string localCandidateStr;
+    private string remoteDescriptionStr;
 
     void Start()
     {
+        peerConnection = null;
+        dataChannel = null;
+        localDescriptionStr = "";
+        localCandidateStr = "";
+        remoteDescriptionStr = "";
+
         DataChannelPluginUtils.InitLogger(RtcLogLevel.RTC_LOG_DEBUG);
         // See https://github.com/paullouisageneau/libdatachannel/issues/275
         // for how to let libdatachannel know about the ICE servers.
@@ -19,13 +28,29 @@ public class CopyPasteOfferer : MonoBehaviour
         peerConnection.SetLocalDescription("offer");
     }
 
-    void OnLocalDescription(Description description)
+    void OnApplicationQuit()
     {
-        print("description:\n" + description.sdp);
+        DataChannelPluginUtils.Cleanup();
     }
 
-    void OnLocalCandidate(Candidate candidate)
+    void OnGUI()
     {
-        print("candidate:\n" + candidate.cand);
+        GUI.TextArea(new Rect(10, 10, 400, 400), localDescriptionStr);
+        GUI.TextArea(new Rect(10, 420, 400, 200), localCandidateStr);
+
+        GUI.Label(new Rect(420, 10, 200, 20), "Enter Remote Description Here");
+        remoteDescriptionStr = GUI.TextArea(new Rect(420, 40, 400, 400), remoteDescriptionStr);
+        if (GUI.Button(new Rect(420, 450, 400, 50), "Set Remote Description"))
+            peerConnection.SetRemoteDescription(remoteDescriptionStr, "");
+    }
+
+    private void OnLocalDescription(Description description)
+    {
+        localDescriptionStr = "Local Description(Paste this to the other peer):\n" + description.sdp;
+    }
+
+    private void OnLocalCandidate(Candidate candidate)
+    {
+        localCandidateStr += "Local Candidate(Paste this to the other peer after the local description):\n" + candidate.cand + "\n\n";
     }
 }
