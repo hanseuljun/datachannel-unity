7d3017d2561a1eaca3ec9b59fa1ee28c1542fce5
Hanseul Jun <hanseuljun@gmail.com>
2021-03-11 01:20:40 -0800

    captialize variables

--- scripts/cmake-vs.ps1
+++ scripts/cmake-vs.ps1
@@ -1,12 +1,12 @@
-$sourcePath = $PSScriptRoot + "\.."
-$buildPath = $PSScriptRoot + "\..\build"
+$SourcePath = $PSScriptRoot + "\.."
+$BuildPath = $PSScriptRoot + "\..\build"
 
-New-Item $buildPath -ItemType Directory
+New-Item $BuildPath -ItemType Directory
 
 $CMakeToolchainFile = $PSScriptRoot + "\..\vcpkg\scripts\buildsystems\vcpkg.cmake"
 
-cmake -S $sourcePath -B $buildPath\arm64-uwp -G "Visual Studio 16 2019" -A ARM64 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0" -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DNO_EXAMPLES=ON -DNO_TESTS=ON -DRSA_KEY_BITS_2048=ON -DCAPI_STDCALL=ON
+cmake -S $SourcePath -B $BuildPath\arm64-uwp -G "Visual Studio 16 2019" -A ARM64 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0" -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DNO_EXAMPLES=ON -DNO_TESTS=ON -DRSA_KEY_BITS_2048=ON -DCAPI_STDCALL=ON
 
-cmake -S $sourcePath -B $buildPath\x64 -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DCAPI_STDCALL=ON
+cmake -S $SourcePath -B $BuildPath\x64 -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DCAPI_STDCALL=ON
 
-cmake -S $sourcePath -B $buildPath\x86-uwp -G "Visual Studio 16 2019" -A Win32 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0" -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DNO_EXAMPLES=ON -DNO_TESTS=ON -DRSA_KEY_BITS_2048=ON -DCAPI_STDCALL=ON
+cmake -S $SourcePath -B $BuildPath\x86-uwp -G "Visual Studio 16 2019" -A Win32 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0" -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DNO_EXAMPLES=ON -DNO_TESTS=ON -DRSA_KEY_BITS_2048=ON -DCAPI_STDCALL=ON
